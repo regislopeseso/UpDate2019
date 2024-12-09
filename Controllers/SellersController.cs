068963c4f22a1087de4d360adcf2ef63bde923cc
Regis <regis.lopes.eso@gmail.com>
2024-12-09 14:04:47 -0300

    Delete Seller

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -26,6 +26,7 @@ namespace SalesWebMvc.Controllers
             var viewModel = new SellerFormViewModell { Departments = departments };
             return View(viewModel);
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
@@ -34,6 +35,29 @@ namespace SalesWebMvc.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
 
     }
 }
